f826570381204c3e4dc42eea5e5f806919bd9e70
Prarthan_Akavn <akavnsolutechllp1@gmail.com>
2025-02-21 11:47:14 +0530

    username updation

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import mozzarella from '/MenuQRFrontPage.jpg';
 
 const MainContent = () => {
-  const pdfUrl = 'https://akavn123.github.io/mrmozzarella/MrMozzarelaMenu.pdf';
+  const pdfUrl = 'https://akavnsolutechllp.github.io/mrmozzarella/MrMozzarelaMenu.pdf';
 
   return (
     <div className='h-auto w-full flex flex-col justify-center items-center gap-4 p-2 rounded-xl'>
       <div className='h-auto w-full xl:w-[78%] lg:w-[90%] 2xl:w-[84%] flex justify-center items-center bg-white p-1 rounded-md'>
         <img src={mozzarella} alt="Menu Preview" className='shadow-lg xl:w-[100%]' />
       </div>
-
       <div className='w-full h-auto flex justify-center items-center gap-4'>
         {/* View PDF */}
         <a 
           href={pdfUrl} 
-          target="_self"
+          target="_blank"
           rel="noopener noreferrer"
           className='bg-white py-2 md:py-3 lg:py-4 xl:py-2 2xl:py-3 px-6 md:px-8 font-poppins md:text-lg lg:text-xl xl:text-base font-semibold text-[#723727] shadow-xl'
         >
@@ -23,7 +22,7 @@ const MainContent = () => {
         {/* Download PDF */}
         <a 
           href={pdfUrl} 
-          download="Menu For QR.pdf"
+          download="MrMozzarelaMenu.pdf"
           className='bg-white py-2 md:py-3 lg:py-4 xl:py-2 2xl:py-3 px-6 md:px-8 font-poppins md:text-lg lg:text-xl xl:text-base font-semibold text-[#723727] shadow-xl'
         >
           Download Pdf
